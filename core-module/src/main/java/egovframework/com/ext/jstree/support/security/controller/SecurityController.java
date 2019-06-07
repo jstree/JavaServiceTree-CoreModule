cda948dac69b47bc140da3151ee29d61adbf06af
LeeDongMin <admin@313.co.kr>
2019-06-07 17:35:14 +0900

    [ jsTree Service Framework(TSF) csrf seperate ]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF의 csrf 분리.
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/SecurityController.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/SecurityController.java
@@ -3,6 +3,12 @@ package egovframework.com.ext.jstree.support.security.controller;
 import egovframework.com.ext.jstree.support.mvc.GenericAbstractController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping(value = {"/api/jsTreeServiceFramework/security"})
@@ -12,4 +18,18 @@ public class SecurityController extends GenericAbstractController {
     public String jsTreeCSRFtoJson() {
         return "egovframework/com/ext/jstree/csrf";
     }
+
+    @RequestMapping("/getCSRF.do")
+    public ModelAndView jsTreeGetCSRFtoJson(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+
+        String token = request.getParameter("X-CSRF-TOKEN");
+        if (token == null || "".equals(token)) {
+            token = request.getHeader("X-CSRF-TOKEN");
+        }
+
+        ModelAndView modelAndView = new ModelAndView("jsonView");
+        modelAndView.addObject("result", token);
+        return modelAndView;
+    }
+
 }
