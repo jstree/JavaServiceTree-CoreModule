823d57b373afdb8eebc9d80a1eeabea0313fbde6
LeeDongMin <admin@313.co.kr>
2019-06-11 10:59:27 +0900

    [ jsTree Service Framework(TSF) csrf 처리를 완벽하게 처리하지 못하여 변경함 ]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF의 csrf 분리가 되지 않아서 원복하고 버전을 변경함. 강의 데모용
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/SecurityController.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/SecurityController.java
@@ -1,41 +1,15 @@
 package egovframework.com.ext.jstree.support.security.controller;
 
 import egovframework.com.ext.jstree.support.mvc.GenericAbstractController;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 @RequestMapping(value = {"/api/jsTreeServiceFramework/security"})
 public class SecurityController extends GenericAbstractController {
 
-    public static final String DEFAULT_CSRF_TOKEN_ATTR_NAME = HttpSessionCsrfTokenRepository.class.getName().concat(".CSRF_TOKEN");
-
     @RequestMapping("/csrf.do")
     public String jsTreeCSRFtoJson() {
         return "egovframework/com/ext/jstree/csrf";
     }
-
-    @RequestMapping("/getCSRF.do")
-    public ModelAndView jsTreeGetCSRFtoJson(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
-
-        String token = request.getParameter("X-CSRF-TOKEN");
-        if (token == null || "".equals(token)) {
-            token = request.getHeader("X-CSRF-TOKEN");
-        }
-
-        CsrfToken sessionToken = (CsrfToken) request.getSession().getAttribute(DEFAULT_CSRF_TOKEN_ATTR_NAME);
-
-        ModelAndView modelAndView = new ModelAndView("jsonView");
-        modelAndView.addObject("result", sessionToken.getToken().toString());
-        return modelAndView;
-    }
-
 }
