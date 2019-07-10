4987929cac9e36aa7f7e3eba63e6226c208f5330
LeeDongMin <admin@313.co.kr>
2019-07-10 21:05:42 +0900

    [ jsTree Service Framework(TSF) 로그인페이지 처리 ]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF에 egovframework 의 로그인 페이지를 처리합니다.
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/cmm/web/EgovComIndexController.java
+++ core-module/src/main/java/egovframework/com/cmm/web/EgovComIndexController.java
@@ -1,7 +1,5 @@
 package egovframework.com.cmm.web;
 
-import egovframework.com.cmm.IncludedCompInfoVO;
-import egovframework.com.ext.jstree.support.util.ParameterParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -10,14 +8,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 @Controller
 public class EgovComIndexController implements ApplicationContextAware, InitializingBean {
@@ -26,20 +21,16 @@ public class EgovComIndexController implements ApplicationContextAware, Initiali
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EgovComIndexController.class);
 
-	private Map<Integer, IncludedCompInfoVO> map;
-
 	public void afterPropertiesSet() throws Exception {}
 
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
-		
 		LOGGER.info("EgovComIndexController setApplicationContext method has called!");
 	}
 
 	@ResponseBody
-	@RequestMapping(value = "/index.do")
+	@RequestMapping(value = "/")
 	public ModelAndView index(ModelMap model, HttpServletRequest request) throws Exception {
-
 		ModelAndView modelAndView = new ModelAndView("jsonView");
 		modelAndView.addObject("result", "jsTree Service Framework is not support frontside page");
 		return modelAndView;
--- core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/PagesController.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/PagesController.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,12 +36,12 @@ public class PagesController {
         return model;
     }
 
+    @ResponseBody
     @RequestMapping(value = {"/accessdenied"}, method = { RequestMethod.GET, RequestMethod.POST })
-    public ModelAndView accessDeniedPage() {
-        ModelAndView model = new ModelAndView();
-        model.addObject("message", "Either username or password is incorrect.");
-        model.setViewName("egovframework/com/ext/jstree/support/security/accessdenied");
-        return model;
+    public ModelAndView accessDeniedPage(ModelMap model, HttpServletRequest request) throws Exception {
+        ModelAndView modelAndView = new ModelAndView("jsonView");
+        modelAndView.addObject("result", "Either username or password is incorrect");
+        return modelAndView;
     }
 
     private String getUser() {
