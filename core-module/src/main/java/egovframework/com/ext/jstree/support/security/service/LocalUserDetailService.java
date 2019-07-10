3fc9486c5ed5618374f0e3e137b511789663525c
LeeDongMin <admin@313.co.kr>
2019-07-10 15:20:42 +0900

    [ jsTree Service Framework(TSF) 반영을 위하여 버전 업그레이드 ]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF에 egovframework 의 BBS 관련 패키지는 필요하지 않아서 삭제 처리합니다.
    오타가 포함되어 한번 더 푸쉬합니다.
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/ext/jstree/support/security/service/LocalUserDetailService.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/support/security/service/LocalUserDetailService.java
@@ -4,6 +4,7 @@ import egovframework.com.ext.jstree.support.security.database.dao.UserDao;
 import egovframework.com.ext.jstree.support.security.database.model.Role;
 import egovframework.com.ext.jstree.support.security.database.model.User;
 import egovframework.com.ext.jstree.support.security.dto.LocalUser;
+import egovframework.com.ext.jstree.support.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,9 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service("localUserDetailService")
 public class LocalUserDetailService implements UserDetailsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocalUserDetailService.class);
 
     @Autowired
     private UserDao userDao;
@@ -23,6 +28,11 @@ public class LocalUserDetailService implements UserDetailsService {
     @Override
     @Transactional
     public LocalUser loadUserByUsername(final String userId) throws UsernameNotFoundException {
+
+        if(StringUtils.isEmpty(userId)){
+            LOGGER.error("사용자 정보가 없는 요청 ( userid empty )" + userId);
+        }
+
         User user = userDao.get(userId);
         if (user == null) {
             return null;
