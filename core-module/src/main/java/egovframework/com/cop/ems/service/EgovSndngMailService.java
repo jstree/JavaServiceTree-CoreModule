8642133598f5ef75827a9d9db353194f7fa22cc0
LeeDongMin <admin@313.co.kr>
2019-07-10 11:29:59 +0900

    [ jsTree Service Framework(TSF) 의 cop 패키지 및 bbs 관련 패키지 소스 삭제 ]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF에 egovframework 의 BBS 관련 패키지는 필요하지 않아서 삭제 처리합니다.
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/cop/ems/service/EgovSndngMailService.java
+++
@@ -1,6 +0,0 @@
-package egovframework.com.cop.ems.service;
-
-public interface EgovSndngMailService {
-
-	boolean sndngMail(SndngMailVO vo) throws Exception;
-}
--- core-module/src/main/java/egovframework/com/utl/wed/filter/FileSaveManager.java
+++
@@ -1,8 +0,0 @@
-package egovframework.com.utl.wed.filter;
-
-import org.apache.commons.fileupload.FileItem;
-
-public interface FileSaveManager {
-    String saveFile(FileItem fileItem, String imageBaseDir, String imageDomain);
-}
-
