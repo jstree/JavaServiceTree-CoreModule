fe3f6d3d1cdd6b19569c45cd650f384079e79609
LeeDongMin <admin@313.co.kr>
2019-07-10 13:31:55 +0900

    [ jsTree Service Framework(TSF) 의 cop 패키지 및 bbs 관련 패키지 소스 삭제 ]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF에 egovframework 의 BBS 관련 패키지는 필요하지 않아서 삭제 처리합니다.
    오타가 포함되어 한번 더 푸쉬합니다.
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/cmm/web/EgovComIndexController.java
+++ core-module/src/main/java/egovframework/com/cmm/web/EgovComIndexController.java
@@ -1,14 +1,6 @@
 package egovframework.com.cmm.web;
 
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.TreeMap;
-
 import egovframework.com.cmm.IncludedCompInfoVO;
-import egovframework.com.cmm.LoginVO;
-import egovframework.com.cmm.annotation.IncludedInfo;
-import egovframework.com.cmm.util.EgovUserDetailsHelper;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -18,6 +10,8 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 public class EgovComIndexController implements ApplicationContextAware, InitializingBean {
@@ -38,105 +32,7 @@ public class EgovComIndexController implements ApplicationContextAware, Initiali
 
 	@RequestMapping("/index.do")
 	public String index(ModelMap model) {
-		return "egovframework/com/cmm/EgovUnitMain";
+		return "/";
 	}
 
-	@RequestMapping("/EgovTop.do")
-	public String top() {
-		return "egovframework/com/cmm/EgovUnitTop";
-	}
-
-	@RequestMapping("/EgovBottom.do")
-	public String bottom() {
-		return "egovframework/com/cmm/EgovUnitBottom";
-	}
-
-	@RequestMapping("/EgovContent.do")
-	public String setContent(ModelMap model) {
-
-		LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
-		model.addAttribute("loginVO", loginVO);
-
-		return "egovframework/com/cmm/EgovUnitContent";
-	}
-
-	@RequestMapping("/EgovLeft.do")
-	public String setLeftMenu(ModelMap model) {
-
-		/* 최초 한 번만 실행하여 map에 저장해 놓는다. */
-		if (map == null) {
-			map = new TreeMap<Integer, IncludedCompInfoVO>();
-			RequestMapping rmAnnotation;
-			IncludedInfo annotation;
-			IncludedCompInfoVO zooVO;
-
-			/*
-			 * EgovLoginController가 AOP Proxy되는 바람에 클래스를 reflection으로 가져올 수 없음
-			 */
-			try {
-				Class<?> loginController = Class.forName("egovframework.com.uat.uia.web.EgovLoginController");
-				Method[] methods = loginController.getMethods();
-				for (int i = 0; i < methods.length; i++) {
-					annotation = methods[i].getAnnotation(IncludedInfo.class);
-
-					if (annotation != null) {
-						LOGGER.debug("Found @IncludedInfo Method : {}", methods[i]);
-						zooVO = new IncludedCompInfoVO();
-						zooVO.setName(annotation.name());
-						zooVO.setOrder(annotation.order());
-						zooVO.setGid(annotation.gid());
-
-						rmAnnotation = methods[i].getAnnotation(RequestMapping.class);
-						if ("".equals(annotation.listUrl()) && rmAnnotation != null) {
-							zooVO.setListUrl(rmAnnotation.value()[0]);
-						} else {
-							zooVO.setListUrl(annotation.listUrl());
-						}
-						map.put(zooVO.getOrder(), zooVO);
-					}
-				}
-			} catch (ClassNotFoundException e) {
-				LOGGER.error("No egovframework.com.uat.uia.web.EgovLoginController!!");
-			}
-			/* 여기까지 AOP Proxy로 인한 코드 */
-
-			/*@Controller Annotation 처리된 클래스를 모두 찾는다.*/
-			Map<String, Object> myZoos = applicationContext.getBeansWithAnnotation(Controller.class);
-			LOGGER.debug("How many Controllers : ", myZoos.size());
-			for (final Object myZoo : myZoos.values()) {
-				Class<? extends Object> zooClass = myZoo.getClass();
-
-				Method[] methods = zooClass.getMethods();
-				LOGGER.debug("Controller Detected {}", zooClass);
-				for (int i = 0; i < methods.length; i++) {
-					annotation = methods[i].getAnnotation(IncludedInfo.class);
-
-					if (annotation != null) {
-						//LOG.debug("Found @IncludedInfo Method : " + methods[i] );
-						zooVO = new IncludedCompInfoVO();
-						zooVO.setName(annotation.name());
-						zooVO.setOrder(annotation.order());
-						zooVO.setGid(annotation.gid());
-						/*
-						 * 목록형 조회를 위한 url 매핑은 @IncludedInfo나 @RequestMapping에서 가져온다
-						 */
-						rmAnnotation = methods[i].getAnnotation(RequestMapping.class);
-						if ("".equals(annotation.listUrl())) {
-							zooVO.setListUrl(rmAnnotation.value()[0]);
-						} else {
-							zooVO.setListUrl(annotation.listUrl());
-						}
-
-						map.put(zooVO.getOrder(), zooVO);
-					}
-				}
-			}
-		}
-
-		model.addAttribute("resultList", map.values());
-		
-		LOGGER.debug("EgovComIndexController index is called ");
-
-		return "egovframework/com/cmm/EgovUnitLeft";
-	}
 }
