d7088cf9a124e0e4e47d544aeae935787cadf325
LeeDongMin <admin@313.co.kr>
2019-07-19 18:30:40 +0900

    [ jsTree Service Framework(TSF) localuser add after return fix ]
    [SmartCommit] SP-548 #close #time 1h +review SR @dongmin.lee
    #comment localuser add after return fix
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/RegistrationController.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/RegistrationController.java
@@ -1,19 +1,11 @@
 package egovframework.com.ext.jstree.support.security.controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import egovframework.com.ext.jstree.support.security.dto.LocalUser;
 import egovframework.com.ext.jstree.support.security.dto.UserRegistrationForm;
 import egovframework.com.ext.jstree.support.security.exception.UserAlreadyExistAuthenticationException;
 import egovframework.com.ext.jstree.support.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RegistrationController {
