3e80c66d7ee106dc4bb03f126d02dc4e9de932e1
LeeDongMin <admin@313.co.kr>
2019-07-22 10:21:02 +0900

    [ jsTree Service Framework(TSF) regist controller change]
    [SmartCommit] SP-548 #close #time 1h +review SR @dongmin.lee
    #comment 사용자 추가 컨트롤러 조정 및 필요없는 메소드 삭제.
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/RegistrationController.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/support/security/controller/RegistrationController.java
@@ -1,19 +1,12 @@
 package egovframework.com.ext.jstree.support.security.controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import egovframework.com.ext.jstree.support.security.dto.LocalUser;
 import egovframework.com.ext.jstree.support.security.dto.UserRegistrationForm;
 import egovframework.com.ext.jstree.support.security.exception.UserAlreadyExistAuthenticationException;
 import egovframework.com.ext.jstree.support.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class RegistrationController {
@@ -21,16 +14,9 @@ public class RegistrationController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public ModelAndView signup(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
-        ModelAndView model = new ModelAndView();
-        model.addObject("title", "User Registration Form");
-        model.setViewName("egovframework/com/ext/jstree/support/security/registration");
-        return model;
-    }
-
+    @ResponseBody
     @RequestMapping(value = {"/user/register"}, method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public @ResponseBody String registerUser(@RequestBody UserRegistrationForm registrationForm) throws UserAlreadyExistAuthenticationException {
+    public ModelAndView registerUser(@RequestBody UserRegistrationForm registrationForm) throws UserAlreadyExistAuthenticationException {
 
         if (registrationForm.getUserId() == null) {
             registrationForm.setUserId(registrationForm.getUserId());
@@ -38,7 +24,8 @@ public class RegistrationController {
 
         LocalUser localUser = (LocalUser) userService.registerNewUser(registrationForm);
 
-       return "success";
-
+        ModelAndView modelAndView = new ModelAndView("jsonView");
+        modelAndView.addObject("result", "add user success");
+        return modelAndView;
     }
 }
