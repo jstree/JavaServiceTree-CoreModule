e968b2d30c839fd6fb37469ec1cba71732788ec3
LeeDongMin <admin@313.co.kr>
2019-09-23 19:28:37 +0900

    [ jsTree Service Framework(TSF) : Core-Module]
    [SmartCommit] SP-581 #close #time 1h +review SR @dongmin.lee
    #comment index controller api list 기능 추가
    
    [Document] http://www.313.co.kr/confluence/display/WIKI
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-582
    [VersionControl] https://github.com/jstree/JavaServiceTree-CoreModule
    [CodeReview] http://www.313.co.kr/fecru/project/SR
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/5694
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/main/java/egovframework/com/cmm/web/EgovComIndexController.java
+++ core-module/src/main/java/egovframework/com/cmm/web/EgovComIndexController.java
@@ -1,19 +1,39 @@
 package egovframework.com.cmm.web;
-
+import egovframework.com.cmm.annotation.IncludedInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ *
+ * <pre>
+ *    ******************************************************************************
+ *    (C) 2013 - 2019 by 313 DEV GRP, Inc - All Rights Reserved
+ *    Unauthorized copying of this file, via any medium is strictly prohibited
+ *    Written by 313 developer group ( 313@313.co.kr ) , December 2013
+ *    ******************************************************************************
+ *
+ *    메인 인덱스 컨트롤러이며, IncludeInfo 커스텀 어노테이션을 Controller 어노테이션을 기반으로 수집하여 json으로 제공한다.
+ * </pre>
+ *
+ * @author 313 DEV GRP
+ */
 @Controller
 public class EgovComIndexController implements ApplicationContextAware, InitializingBean {
 
@@ -21,19 +41,58 @@ public class EgovComIndexController implements ApplicationContextAware, Initiali
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EgovComIndexController.class);
 
+	private static Map<String, String> includeInfoAnnotaion = Collections.emptyMap();
+
 	public void afterPropertiesSet() throws Exception {}
 
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
+		findAnnotatedClasses("egovframework");
 		LOGGER.info("EgovComIndexController setApplicationContext method has called!");
 	}
 
 	@ResponseBody
 	@RequestMapping(value = "/index.do")
 	public ModelAndView index(ModelMap model, HttpServletRequest request) throws Exception {
+
 		ModelAndView modelAndView = new ModelAndView("jsonView");
-		modelAndView.addObject("result", "jsTree Service Framework is not support frontside page");
+		modelAndView.addObject("result", includeInfoAnnotaion);
 		return modelAndView;
 	}
 
+	public void findAnnotatedClasses(String scanPackage) {
+		ClassPathScanningCandidateComponentProvider provider = createComponentScanner();
+		for (BeanDefinition beanDef : provider.findCandidateComponents(scanPackage)) {
+			printMetadata(beanDef);
+		}
+	}
+
+	private ClassPathScanningCandidateComponentProvider createComponentScanner() {
+		// Don't pull default filters (@Component, etc.):
+		ClassPathScanningCandidateComponentProvider provider
+				= new ClassPathScanningCandidateComponentProvider(false);
+		provider.addIncludeFilter(new AnnotationTypeFilter(Controller.class));
+		return provider;
+	}
+
+	private void printMetadata(BeanDefinition beanDef) {
+		try {
+			Class<?> cl = Class.forName(beanDef.getBeanClassName());
+			Method[] methods = cl.getMethods();
+			System.out.println("metthod length ="+methods.length );
+			for(Method method : methods){
+				if (method.isAnnotationPresent(IncludedInfo.class)) {
+					//Should give you expected results
+					System.out.println("metthod name ="+method.getName());
+					includeInfoAnnotaion.put(beanDef.getBeanClassName(), method.getName());
+				}
+			}
+
+			IncludedInfo findable = cl.getAnnotation(IncludedInfo.class);
+			System.out.printf("Found class: %s, with meta name: %s%n",
+					cl.getSimpleName(), findable.name());
+		} catch (Exception e) {
+			System.err.println("Got exception: " + e.getMessage());
+		}
+	}
 }
