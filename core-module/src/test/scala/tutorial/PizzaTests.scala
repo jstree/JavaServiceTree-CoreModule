3ff4fc80ce408a31986723c0d7619eb3bff908ee
LeeDongMin <admin@313.co.kr>
2019-10-25 11:21:03 +0900

    [ jsTree Service Framework(TSF) : Core-Module]
    [SmartCommit] SP-581 #close #time 1h +review SR @dongmin.lee
    #comment async 설정 및 스칼라 유닛테스트 추가. 컴포넌트 스캔 추가
    
    [Document] http://www.313.co.kr/confluence/display/WIKI
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-582
    [VersionControl] https://github.com/jstree/JavaServiceTree-CoreModule
    [CodeReview] http://www.313.co.kr/fecru/project/SR
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/5694
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/test/scala/tutorial/PizzaTests.scala
+++ core-module/src/test/scala/tutorial/PizzaTests.scala
@@ -12,6 +12,30 @@ class PizzaTests extends TestCase {
     pizza = new Pizza
   }
 
+  def testChapter1: Unit = {
+    println(1+2)
+    val res0 = 1 //val res0: Int = 0
+    println(res0 * 313)
+
+    var msg: java.lang.String = "313 DEV GRP"
+    println(msg)
+    msg = "change"
+    println(msg)
+    println(max(313, 131))
+    println(greeting())
+  }
+
+  def max(x:Int, y:Int): Int = {
+    if(x>y)
+      x
+    else
+      y
+  }
+
+  def greeting() = {
+    println("hello scala")
+  }
+
   def testOneTopping {
     pizza.addTopping(Topping("green olives"))
     assertEquals(pizza.getToppings.size, 1)
