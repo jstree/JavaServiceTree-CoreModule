0b1669bbc43cf77d95222027ca7b07849568e1b0
LeeDongMin <admin@313.co.kr>
2019-11-20 13:09:46 +0900

    [ jsTree Service Framework(TSF) : Core-Module]
    [SmartCommit] SP-581 #close #time 1h +review SR @dongmin.lee
    #comment common exec lib import and generic Controller remove abstract controller
    
    [Document] http://www.313.co.kr/confluence/display/WIKI
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-582
    [VersionControl] https://github.com/jstree/JavaServiceTree-CoreModule
    [CodeReview] http://www.313.co.kr/fecru/project/SR
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/5694
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/main/java/egovframework/com/ext/jstree/support/mvc/GenericAbstractController.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/support/mvc/GenericAbstractController.java
@@ -1,28 +1,23 @@
 package egovframework.com.ext.jstree.support.mvc;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import egovframework.com.ext.jstree.support.util.StringUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.ext.jstree.support.util.ParameterParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.AbstractController;
 import org.springmodules.validation.commons.DefaultBeanValidator;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import egovframework.com.cmm.EgovMessageSource;
-import egovframework.com.ext.jstree.support.util.ParameterParser;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class GenericAbstractController{
 
