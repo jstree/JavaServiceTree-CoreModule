383376746bc1db9dba54a42645d25d0028bf6254
LeeDongMin <admin@313.co.kr>
2019-12-14 12:04:51 +0900

    [ jsTree Service Framework(TSF) : Core-Module]
    [SmartCommit] SP-581 #close #time 1h +review SR @dongmin.lee
    #comment ehcache disk 설정 완료. 2019년 TSF Close
    
    [Document] http://www.313.co.kr/confluence/display/WIKI
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-582
    [VersionControl] https://github.com/jstree/JavaServiceTree-CoreModule
    [CodeReview] http://www.313.co.kr/fecru/project/SR
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/5694
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/test/java/egovframework/SpringUnitTestSample.java
+++ core-module/src/test/java/egovframework/SpringUnitTestSample.java
@@ -1,13 +1,20 @@
 package egovframework;
 
 import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Administrator on 2019-10-24.
@@ -21,10 +28,34 @@ public class SpringUnitTestSample extends BaseUnitTest {
     @Qualifier("JsTreeHibernateService")
     private JsTreeHibernateService jsTreeHibernateService;
 
+    public Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Resource(name = "customCacheManager")
+    CacheManager cacheManager;
+
 
     @Test
     public void functionalScannerTest() {
         System.out.println("springTestStart");
+    }
+
+    @Test
+    public void chacheTest(){
+        Cache tmpcache = cacheManager.getCache("LocalEhCache");
+
+
+        String urlCacheKey = "/api/v1/test" + "/engine/2019.12.01.03/2019-12/48967";
+        String strCacheValue = "test Value";
+
+        tmpcache.put(new Element(urlCacheKey, strCacheValue));
+        Element value = tmpcache.get(urlCacheKey);
+        logger.info(value.getValue().toString());
+
+        tmpcache.remove(urlCacheKey);
+
+        tmpcache.put(new Element(urlCacheKey, strCacheValue));
+
+
     }
 
 
